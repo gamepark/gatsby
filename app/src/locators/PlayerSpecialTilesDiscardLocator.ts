d1a9d3a12957092e963bfd5f3dd2722f49ca685c
Romain Fromi <romain.fromi@gmail.com>
2025-05-14 09:54:27 +0200

    Remove PlayerSpecialTilesDiscardDescription to simplify code and get a border radius on the drop area

--- app/src/locators/PlayerSpecialTilesDiscardLocator.ts
+++ app/src/locators/PlayerSpecialTilesDiscardLocator.ts
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { PlayerRole } from '@gamepark/gatsby/PlayerRole'
-import { DeckLocator, DropAreaDescription } from '@gamepark/react-game'
+import { DeckLocator } from '@gamepark/react-game'
 import { Location } from '@gamepark/rules-api'
 
 class PlayerSpecialTilesDiscardLocator extends DeckLocator {
@@ -14,13 +14,6 @@ class PlayerSpecialTilesDiscardLocator extends DeckLocator {
   getRotateZ(location: Location) {
     return location.player === PlayerRole.Dorothy ? -90 : 90
   }
-
-  locationDescription = new PlayerSpecialTilesDiscardDescription()
-}
-
-export class PlayerSpecialTilesDiscardDescription extends DropAreaDescription {
-  height = 2.5
-  width = 5.5
 }
 
 export const playerSpecialTilesDiscardLocator = new PlayerSpecialTilesDiscardLocator()

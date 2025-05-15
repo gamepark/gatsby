5acc3a16a574652c63aefe944e14c9085ea999bd
Romain Fromi <romain.fromi@gmail.com>
2025-05-15 15:53:01 +0200

    Change DnD collision algorithm to prevent miss drop when switching influence tokens

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,4 +1,5 @@
 /** @jsxImportSource @emotion/react */
+import { pointerWithin } from '@dnd-kit/core'
 import { css } from '@emotion/react'
 import { GameTable } from '@gamepark/react-game'
 import { FC } from 'react'
@@ -12,7 +13,15 @@ export const GameDisplay: FC<GameDisplayProps> = () => {
   const margin = { top: 7, left: 0, right: 0, bottom: 0 }
   return (
     <>
-      <GameTable xMin={-38} xMax={38} yMin={-18.5} yMax={18.5} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
+      <GameTable
+        xMin={-38}
+        xMax={38}
+        yMin={-18.5}
+        yMax={18.5}
+        margin={margin}
+        css={process.env.NODE_ENV === 'development' && tableBorder}
+        collisionAlgorithm={pointerWithin}
+      >
         <PlayerPanels />
       </GameTable>
     </>
